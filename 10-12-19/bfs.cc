f91d2e78f782333454ea0a4ee07aa01c89a89e0a
2004Anton <100023295@mvla.net>
2019-10-12 15:03:22 -0700

    initial draft

---
+++ 10-12-19/bfs.cc
@@ -0,0 +1,9 @@
+// Breath First Search
+// Graph traversals
+// Graph traversal means visiting every vertex and edge exactly once in a well-defined order. 
+// While using certain graph algorithms, you must ensure that each vertex of the graph is visited exactly once. 
+// The order in which the vertices are visited are important and may depend upon the algorithm or question that you are solving.
+// During a traversal, it is important that you track which vertices have been visited. The most common way of tracking vertices is 
+// to mark them.
+// https://www.redblobgames.com/pathfinding/a-star/implementation.html
+// https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/
