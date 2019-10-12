aaca00f6786f94a109b417789a154a5acccf7bc3
2004Anton <100023295@mvla.net>
2019-10-12 15:05:41 -0700

    updated with tree definition.

--- 10-12-19/bfs.cc
+++ 10-12-19/bfs.cc
@@ -7,3 +7,55 @@
 // to mark them.
 // https://www.redblobgames.com/pathfinding/a-star/implementation.html
 // https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/
+
+
+using namespace std;
+
+class SimpleGraph {
+  map<char, vector<char>> edges;
+
+public:
+  SimpleGraph(map<char, vector<char>> &&m) { edges = m; };
+  vector<char> neighbors(char id) { return edges[id]; };
+};
+
+// SimpleGraph g{{{'a', {'b'}},
+//                {'b', {'a', 'c', 'd'}},
+//                {'c', {'a'}},
+//                {'d', {'a', 'e'}},
+//                {'e', {'b'}}}};
+
+void bfs(SimpleGraph &my_graph, char start) {
+
+  
+}
+int main() {
+
+  map<char, vector<char>> m;
+  vector<char> t{'b'};
+  m.insert(pair<char, vector<char>>('a', t));
+  // g['a'] = t;
+  t = {'a', 'c', 'd'};
+  m.insert(pair<char, vector<char>>('b', t));
+  // g['b'] = t;
+  t = {'a'};
+  // g['c'] = t;
+  m.insert(pair<char, vector<char>>('c', t));
+  t = {'a', 'e'};
+  // g['d'] = t;
+  m.insert(pair<char, vector<char>>('d', t));
+  t = {'b'};
+  // g['e'] = t;
+  m.insert(pair<char, vector<char>>('e', t));
+
+  bfs(g, 'a');
+
+  bfs(g, 'c');
+  return 0;
+}
+
+
+
+
+
+
